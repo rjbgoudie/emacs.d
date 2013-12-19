62daeabf2d1e405b1f2be2be4b0c6396c1d532b0
rjbgoudie <rjbgoudie@users.noreply.github.com>
2013-12-19 14:42:32 +0000

    Hide emacs' backups etc in ~/.dotfiles/emacs.d/backups

--- init.el
+++ init.el
@@ -1,1 +1,15 @@
 (menu-bar-mode -1)
+
+;; https://github.com/technomancy/emacs-starter-kit/blob/master/init.el#L20
+(setq dotfiles-dir (file-name-directory
+                   (or (buffer-file-name) load-file-name)))
+
+;; Don't clutter up directories with files~
+;; http://stackoverflow.com/a/2020954
+(setq backup-directory-alist `(("." . ,(expand-file-name
+                                    (concat dotfiles-dir "backups")))))
+
+;; Don't clutter with #files either
+;; http://stackoverflow.com/a/2020954
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name (concat dotfiles-dir "backups")))))
