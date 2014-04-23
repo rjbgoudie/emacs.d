636d0daac351cd2f63dd6585c7b9fedaa3b9359c
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-04-23 17:46:54 +0100

    Nicer display of git-gutter

--- git.el
+++ git.el
@@ -1,2 +1,9 @@
 (require 'git-gutter)
 (global-git-gutter-mode t)
+(setq git-gutter:modified-sign "~ ") ;; two space
+(setq git-gutter:added-sign "+ ")    ;; multiple character is OK
+(setq git-gutter:deleted-sign "-")
+
+(set-face-foreground 'git-gutter:modified "orange")
+(set-face-foreground 'git-gutter:added "green")
+(set-face-foreground 'git-gutter:deleted "red")
