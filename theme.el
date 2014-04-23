829fa3fb99cfedace3204d212002a81e88bc32a5
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-04-23 17:53:54 +0100

    Highlight current line in emacs

--- theme.el
+++ theme.el
@@ -3,3 +3,7 @@
             (modify-frame-parameters
              frame
              '((font . "-*-Consolas-*-*-*-*-13-*-*-*-*-*-*")))))
+
+;; Highlight current line
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#3e4446")
