c8b5e8a56e83b44b66c74112a9aec78ce13dee5c
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-04-23 17:45:37 +0100

    Use fill-column indicator

--- text.el
+++ text.el
@@ -11,3 +11,10 @@
 ; instead use this, as described
 ; https://github.com/alpaker/Fill-Column-Indicator
 (setq whitespace-style '(face trailing))
+
+(require 'fill-column-indicator)
+(setq-default fci-rule-column 80)
+(setq fci-rule-width 2)
+(setq fci-rule-color "#49426c")
+(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+  (global-fci-mode 1)
