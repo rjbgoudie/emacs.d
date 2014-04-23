47afcb3d63cb792addf9dc72ea38f9b69fca6fec
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-04-23 21:57:41 +0100

    Divide emacs text settings into sections

--- text.el
+++ text.el
@@ -1,3 +1,4 @@
+;;; WHITESPACE
 (set-default 'indicate-empty-lines t)
 (setq-default show-trailing-whitespace t)
 (setq require-final-newline t)
@@ -13,6 +14,7 @@
 ; https://github.com/alpaker/Fill-Column-Indicator
 (setq whitespace-style '(face trailing))
 
+;;; TEXT WIDTH
 (require 'fill-column-indicator)
 (setq-default fci-rule-column 80)
 (setq fci-rule-width 2)
@@ -20,8 +22,11 @@
 (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
   (global-fci-mode 1)
 
+;;; PARENTHESES
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
+
+;;; TABS
 ;; Prevent Extraneous Tabs
 ;; https://www.gnu.org/software/emacs/manual/html_node/eintr/
 ;; Indent-Tabs-Mode.html
