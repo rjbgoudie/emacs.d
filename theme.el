fbf8e8cc2f3e344ed13619800c7405e73d263183
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-04-23 21:27:51 +0100

    Use red and green in magit diffs in emacs

--- theme.el
+++ theme.el
@@ -7,3 +7,13 @@
 ;; Highlight current line
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "#3e4446")
+
+
+;; change magit diff colors
+;; http://readystate4.com/2011/02/22/emacs-changing-magits-default-diff-colors
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")
+     (when (not window-system)
+       (set-face-background 'magit-item-highlight "black"))))
