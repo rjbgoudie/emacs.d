459156c369c7ea689f85c60abe8d0281e0deda04
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-09-25 11:11:41 +0100

    stats changes to emacs

--- stats.el
+++ stats.el
@@ -10,13 +10,37 @@
 
 (add-hook 'ess-R-post-run-hook 'ess-execute-screen-options)
 
+(setq ess-roxy-str "#'")
+
 (defun my-ac-ess-config ()
   (setq ac-sources
         (append '(ac-source-R
                   ac-source-filename
                   ac-source-words-in-buffer
                   ac-source-files-in-current-dir)
-                ac-sources)))
+                ac-sources))
+  ;; https://stat.ethz.ch/pipermail/ess-help/2010-September/006384.html
+  ;; (make-local-variable 'adaptive-fill-regexp)
+  ;; (setq adaptive-fill-regexp (concat ess-roxy-str adaptive-fill-regexp))
+  ;; (make-local-variable 'adaptive-fill-first-line-regexp)
+  ;; (setq adaptive-fill-first-line-regexp
+  ;;       (concat ess-roxy-str
+  ;;               adaptive-fill-first-line-regexp))
+  ;; (make-local-variable 'paragraph-start)
+  ;; (setq paragraph-start (concat "\\(" ess-roxy-str "\\)*" paragraph-start))
+  ;; (make-local-variable 'paragraph-separate)
+  ;; (setq paragraph-separate
+  ;;       (concat "\\(" ess-roxy-str "\\)*" paragraph-separate))
+  (set-fill-column 80)
+  (auto-fill-mode)
+  ;; http://stackoverflow.com/a/7502689
+  (add-hook 'local-write-file-hooks
+            (lambda ()
+              (ess-nuke-trailing-whitespace)))
+  )
+
+;; http://stackoverflow.com/a/7502689
+(setq ess-nuke-trailing-whitespace-p 'ask)
 
 (add-hook 'ess-mode-hook 'my-ac-ess-config)
 (add-hook 'ess-post-run-hook 'my-ac-ess-config)
@@ -31,6 +55,8 @@
 (setq comint-scroll-show-maximum-output t)
 
 (setq ess-fancy-comments nil)
+;; http://ess.r-project.org/Manual/ess.html#index-ess_002dsmart_002dcomma
+(setq ess-R-smart-operators t)
 
 ;; Formatting of R code
 ;; https://gist.github.com/crowding/9075782
@@ -42,9 +68,9 @@
 
 (ess-add-style 'rjbg-style
   '((ess-indent-level . 2)
-  (ess-arg-function-offset . nil)
-  (ess-arg-function-offset-new-line . '(2))
-  (ess-expression-offset . 2)
+    (ess-arg-function-offset . nil)
+    (ess-arg-function-offset-new-line . '(2))
+    (ess-expression-offset . 2)
   ))
 
 ;; Flymake
@@ -84,5 +110,8 @@
 
 ;;; R-AUTOYAS
 ;; https://github.com/mlf176f2/r-autoyas.el
-(require 'r-autoyas)
-(add-hook 'ess-mode-hook 'r-autoyas-ess-activate)
+;; (require 'r-autoyas)
+;; (add-hook 'ess-mode-hook 'r-autoyas-ess-activate)
+
+;;; BUGS
+(require 'ess-bugs-d)
