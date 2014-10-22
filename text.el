24bfa9ea0fc74bddfe752c597340e99fb44f32db
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-10-22 10:54:57 +0100

    Disable multiple cursors
    Seems to be causing problems, at least on hpc

--- text.el
+++ text.el
@@ -62,11 +62,11 @@
 
 ;;; MULTIPLE CURSORS
 ;; https://github.com/magnars/multiple-cursors.el
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+;; (require 'multiple-cursors)
+;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+;; (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+;; (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+;; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 ;; autopair messes with multiple cursors, so disable it
 (add-hook 'multiple-cursors-mode-enabled-hook (lambda ()
