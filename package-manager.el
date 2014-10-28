a5c9e47d3ffc2ab527a307b5eea390d8be541dfc
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-10-28 09:54:35 +0000

    Hack emacs package management to work with 24.4
    I have not fixed exclude lists for emacs 24.4 yet

--- package-manager.el
+++ package-manager.el
@@ -1,16 +1,16 @@
 (require 'package)
+(setq package-enable-at-startup nil)
 (package-initialize)
 
-(unless (package-installed-p 'package-filter)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+  (unless (package-installed-p 'package-filter)
   (progn
     (switch-to-buffer
      (url-retrieve-synchronously
       "https://raw.github.com/milkypostman/package-filter/master/package-filter.el"))
     (package-install-from-buffer (package-buffer-info) 'single)))
-(require 'package-filter)
-
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+  (require 'package-filter))
 
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
@@ -21,10 +21,38 @@
 
 ;; (package-refresh-contents)
 
-; ac-math is not on marmalade
-(setq package-archive-exclude-alist '(("melpa" auto-complete auctex color-theme magit git-gutter yasnippet fill-column-indicator flymake autopair r-autoyas buffer-move)))
+;; I have not set-up exclusion lists for emacs 24 yet
+(when (< emacs-major-version 24)
+  ; ac-math is not on marmalade
+  (setq package-archive-exclude-alist '(("melpa"
+                                         auto-complete
+                                         auctex
+                                         color-theme
+                                         magit
+                                         git-gutter
+                                         yasnippet
+                                         fill-column-indicator
+                                         flymake
+                                         autopair
+                                         r-autoyas
+                                         buffer-move))))
 
-(defvar my-packages '(auto-complete ess ac-math auctex color-theme magit git-gutter yasnippet fill-column-indicator flymake autopair multiple-cursors phi-search r-autoyas buffer-move)
+(defvar my-packages '(auto-complete
+                      ess
+                      ac-math
+                      auctex
+                      color-theme
+                      magit
+                      git-gutter
+                      yasnippet
+                      fill-column-indicator
+                      flymake
+                      autopair
+                      multiple-cursors
+                      phi-search
+                      r-autoyas
+                      buffer-move
+                      auto-complete-auctex)
   "A list of packages to ensure are installed at launch.")
 
 (defvar my-24-packages '(project-explorer))
