7b470a3456d7d78237f19690ce453dc7fefa5447
rjbgoudie <rjbgoudie@users.noreply.github.com>
2015-06-03 09:29:14 +0100

    ace-window key bindings for emacs

--- ui.el
+++ ui.el
@@ -10,7 +10,12 @@
 ;; http://endlessparentheses.com/upgrading-ace-jump-for-ace-window.html
 (when (>= emacs-major-version 24)
   (setq avi-keys
-      '(?a ?s ?d ?e ?f ?h ?j ?k ?l ?n ?m ?v ?r ?u))
+        '(?a ?s ?d ?e ?f ?h ?j ?k ?l ?n ?m ?v ?r ?u))
   (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l))
   (global-set-key (kbd "M-s")     #'avi-goto-word-1)
-  (global-set-key (kbd "C-x C-o") #'ace-window))
+  (global-set-key (kbd "C-x C-o") #'ace-window)
+  (avy-setup-default)
+  (global-set-key (kbd "M-g g") 'avy-goto-line)
+  (global-set-key (kbd "M-g e") 'avy-goto-word-0)
+  (global-set-key (kbd "M-g w") 'avy-goto-word-1)
+  (global-set-key (kbd "M-g s") 'avy-goto-subword-0))
